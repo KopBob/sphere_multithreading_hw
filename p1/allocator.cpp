2273bd66e92710a746e766508910fad13ce4a8f4
Boris Kopin <kopbob@gmail.com>
2016-04-18 19:00:33 +0300

    remake

--- p1/allocator.cpp
+++ p1/allocator.cpp
@@ -1,143 +1,161 @@
+#include <cstdlib>
+#include <iostream>
 #include "allocator.h"
 
 
-//////////////////////////////
-//    Pointer Class
-//////////////////////////////
-
 Pointer::Pointer() {
-    ptr = shared_ptr<void *>(new void *(nullptr));
-    size = shared_ptr<size_t>(new size_t(0));
-}
-
-Pointer::Pointer(void **p, size_t n) {
-    ptr = shared_ptr<void *>(p);
-    size = shared_ptr<size_t>(new size_t(n));
-}
-
-bool Pointer::operator==(const Pointer &p) const {
-    return (ptr == p.ptr && size == p.size) ? true : false;
-}
-
-bool Pointer::operator<(const Pointer &p) const {
-    return *ptr < p.get();
+    p = std::shared_ptr<void *>(new void *(nullptr));
+    p_size = std::shared_ptr<size_t>(new size_t(0));
 }
 
 
-//////////////////////////////
-//    Allocator Class
-//////////////////////////////
-
-Allocator::Allocator(void *base, size_t size) : base_ptr(base), size(size) {
-    is_occupied = std::vector<bool>(size, 0);
-    pointers = std::vector<Pointer *>();
+Pointer::Pointer(void **_p, size_t *_p_size) {
+    p = std::shared_ptr<void *>(_p);
+    p_size = std::shared_ptr<size_t>(_p_size);
 }
+
+
+Allocator::Allocator(void *base, size_t size) {
+    memory = base;
+    ocupation = std::vector<bool>(size, false);
+}
+
+
+int Allocator::find_pointer(Pointer &p) {
+    int i;
+
+    for (i = 0; i < pointers.size(); ++i)
+        if (p.get() == (*pointers[i]).get() and p.getSize() == p.getSize())
+            break;
+    if (i == pointers.size())
+        i = -1;
+
+    return i;
+}
+
 
 Pointer Allocator::alloc(size_t N) {
-    int block_begin = 0;
-    int free_blocks = 0;
+    int p_begin = 0;
+    size_t k = N;
 
-    bool is_enough_mem = false;
-
-    for (int i = 0; i < size; ++i) {
-        if (is_occupied[i]) {
-            block_begin = i + 1;
-            free_blocks = 0;
-        } else {
-            ++free_blocks;
-        }
-
-        if (free_blocks == N) {
-            is_enough_mem = true;
-            break;
+    for (int i = 0; i < ocupation.size(); ++i) {
+        if (ocupation[i] == false) {
+            p_begin = i;
+            k = N;
+            for (; i < ocupation.size(); ++i) {
+                if (ocupation[i] == false) k--;
+                if (ocupation[i] == true or !k) break;
+            }
+            if (!k) break;
         }
     }
-
-    if (!is_enough_mem)
+    if (k > 0)
         throw AllocError(AllocErrorType::NoMemory, "No memory\n");
 
-    for (int i = 0; i < N; ++i) is_occupied[block_begin + i] = true;
+    std::fill(ocupation.begin() + p_begin,
+              ocupation.begin() + p_begin + N, true);
 
-    void **new_ptr = new void *((char *)base_ptr + block_begin);
+    void **p = new (void *)((char *) memory + p_begin);
 
-    Pointer *new_pointer = new Pointer(new_ptr, N);
-    pointers.push_back(new_pointer);
+    Pointer *pointer = new Pointer(p, new size_t(N));
+    pointers.push_back(pointer);
 
-    return *new_pointer;
+    return *pointer;
 }
 
-void Allocator::realloc(Pointer &p, size_t N) {
-    // if empty
-    if (p.getSize() == 0) {
-        p = alloc(N);
-        return;
-    }
-
-    // shorter
-    int offset = (char *)p.get() - (char *)base_ptr;
-    if (N < p.getSize()) {
-        for (int i = N; i < p.getSize(); ++i) is_occupied[offset + i] = false;
-        p.setSize(N);
-        return;
-    }
-
-    // bigger
-    //// ckeck for shrinkage
-    bool is_shrinkable = true;
-    for (int i = p.getSize(); i < N; ++i)
-        if (is_occupied[offset + i]) {
-            is_shrinkable = false;
-            break;
-        }
-
-    if (is_shrinkable) {
-        for (int i = p.getSize(); i < N; ++i) is_occupied[offset + i] = true;
-        p.setSize(N);
-    } else {
-        Pointer new_ptr = alloc(N);
-        memcpy(new_ptr.get(), p.get(), p.getSize());
-        free(p);
-        p = new_ptr;
-    }
-}
 
 void Allocator::free(Pointer &p) {
-    int idx = -1;
-
-    for (int i = 0; i < pointers.size(); ++i)
-        if (*pointers[i] == p) idx = i;
-
+    int idx = find_pointer(p);
     if (idx == -1)
         throw AllocError(AllocErrorType::InvalidFree, "Invalid Free\n");
 
-    int offset = (char *)p.get() - (char *)base_ptr;
-    for (int i = 0; i < p.getSize(); ++i) is_occupied[offset + i] = false;
+    size_t offset = (char *) p.get() - (char *) memory;
+
+    std::fill(ocupation.begin() + offset,
+              ocupation.begin() + offset + p.getSize(), false);
 
     delete pointers[idx];
     pointers.erase(pointers.begin() + idx);
     p = Pointer(new void *(nullptr), 0);
 }
 
-void Allocator::defrag() {
-    sort(pointers.begin(), pointers.end());
 
-    char *curr_base = (char *)base_ptr;
+void Allocator::realloc(Pointer &p, size_t N) {
+    int idx;
+    size_t start;
+    int required;
 
-    int total_len = 0;
-    for (int i = 0; i < pointers.size(); ++i) {
-        int curr_size = pointers[i]->getSize();
-        
-        memmove(curr_base, pointers[i]->get(), curr_size);
-        pointers[i]->set(curr_base);
-
-        total_len += curr_size;
-        curr_base += curr_size;
+    idx = find_pointer(p);
+    if (idx == -1) {
+        p = alloc(N);
+        return;
     }
 
-    for (int i = 0; i < total_len; ++i) is_occupied[i] = true;
-    for (int i = total_len; i < size; ++i) is_occupied[i] = false;
+    if (idx != -1 and N == p.getSize()) return;
+
+    start = (char *) p.get() - (char *) memory + p.getSize();
+    required = (int) (N - p.getSize());
+
+    if (required < 0) {
+        std::fill(ocupation.begin() + start + required,
+                  ocupation.begin() + start, false);
+        p.setSize(N);
+        return;
+    }
+
+    for (size_t i = start; i < ocupation.size(); ++i) {
+        if (ocupation[i] == false) required--;
+        if (ocupation[i] == true or !required) break;
+    }
+
+    if (!required) {
+        std::fill(ocupation.begin() + start,
+                  ocupation.begin() + start + N - p.getSize(), true);
+        p.setSize(N);
+    } else {
+        Pointer new_p = alloc(N);
+        std::memcpy(new_p.get(), p.get(), p.getSize());
+        Allocator::free(p);
+        p = new_p;
+    }
 }
 
-Allocator::~Allocator() {
-    for (int i = 0; i < pointers.size(); ++i) delete pointers[i];
+
+void Allocator::defrag() {
+    std::sort(pointers.begin(), pointers.end());
+
+    int curr_pos = 0;
+    for (int i = 0; i < pointers.size(); ++i) {
+        std::memcpy((char *)memory + curr_pos, pointers[i]->get(), pointers[i]->getSize());
+        pointers[i]->set((void *) ((char *) memory + curr_pos));
+        curr_pos += pointers[i]->getSize();
+    }
+
+    std::fill(ocupation.begin(),
+              ocupation.begin() + curr_pos, true);
+    std::fill(ocupation.begin() + curr_pos,
+              ocupation.end(), false);
 }
+
+//int main() {
+//    size_t mem_size = 256;
+//    int *mem = (int *) malloc(mem_size * sizeof(int));
+//
+//    Allocator *allocator = new Allocator(mem, mem_size);
+//
+//    Pointer p1 = allocator->alloc(1);
+//    Pointer p2 = allocator->alloc(4);
+//    Pointer p3 = allocator->alloc(3);
+//    Pointer p4 = allocator->alloc(2);
+//    allocator->free(p1);
+//    allocator->free(p3);
+//    allocator->realloc(p2, 5);
+//
+//    allocator->defrag();
+//
+//
+//    delete allocator;
+//    free(mem);
+//
+//    return 1;
+//}
--- p1/allocator.h
+++ p1/allocator.h
@@ -2,20 +2,19 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
 enum class AllocErrorType {
     InvalidFree,
     NoMemory,
 };
 
 class AllocError : std::runtime_error {
-   private:
+private:
     AllocErrorType type;
 
-   public:
-    AllocError(AllocErrorType _type, std::string message)
-        : runtime_error(message), type(_type) {}
+public:
+    AllocError(AllocErrorType _type, std::string message) :
+            runtime_error(message),
+            type(_type) { }
 
     AllocErrorType getType() const { return type; }
 };
@@ -23,37 +22,40 @@ class AllocError : std::runtime_error {
 class Allocator;
 
 class Pointer {
-    shared_ptr<void *> ptr;
-    shared_ptr<size_t> size;
-
-   public:
+    std::shared_ptr<void *> p;
+    std::shared_ptr<size_t> p_size;
+public:
     Pointer();
-    Pointer(void **ptr, size_t n);
 
-    void *get() const { return *ptr; }
-    void set(void *p) { *ptr = p; }
+    Pointer(void **p, size_t *p_size);
 
-    size_t getSize() const { return *size; }
-    void setSize(size_t s) { *size = s; }
+    void *get() const { return *p; }
+    void set(void *_p) {*p = _p;}
 
-    bool operator==(const Pointer &) const;
-    bool operator<(const Pointer &p) const;
+    size_t getSize() const { return *p_size; }
+    void setSize(size_t size) { *p_size = size; }
+
+    bool operator<(const Pointer &o) const {return *p < o.get();}
 };
 
 class Allocator {
-    void *base_ptr;
-    size_t size;
-    std::vector<bool> is_occupied;
+    void *memory;
+    std::vector<bool> ocupation;
     std::vector<Pointer *> pointers;
 
-   public:
+    int find_pointer(Pointer &p);
+
+public:
     Allocator(void *base, size_t size);
-    ~Allocator();
 
     Pointer alloc(size_t N);
+
     void realloc(Pointer &p, size_t N);
+
     void free(Pointer &p);
 
     void defrag();
+
     std::string dump() { return ""; }
 };
+
